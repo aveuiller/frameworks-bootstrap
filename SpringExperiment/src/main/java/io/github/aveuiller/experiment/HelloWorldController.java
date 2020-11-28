71c461666335d74525833792fa6245023dd7f249
Antoine Veuiller <aveuiller@online.net>
2020-11-28 11:16:38 +0100

    feat(spring): add first test, return actual json

--- SpringExperiment/src/main/java/io/github/aveuiller/experiment/HelloWorldController.java
+++ SpringExperiment/src/main/java/io/github/aveuiller/experiment/HelloWorldController.java
@@ -3,10 +3,15 @@ package io.github.aveuiller.experiment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloWorldController {
     @RequestMapping("/hello")
-    public String index() {
-        return "Hello world";
+    public Map<String, String> index() {
+        HashMap<String, String> output = new HashMap<>();
+        output.put("Hello", "World");
+        return output;
     }
 }
---
+++ SpringExperiment/src/test/java/io/github/aveuiller/experiment/HelloWorldControllerTest.java
@@ -0,0 +1,27 @@
+package io.github.aveuiller.experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"Hello\":\"World\"}")));
+    }
+}
