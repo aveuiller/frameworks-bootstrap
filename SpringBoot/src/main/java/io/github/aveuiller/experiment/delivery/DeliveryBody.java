5a77fb253d5bf1359a7dc4adbf737f850353293f
Antoine Veuiller <aveuiller@online.net>
2021-01-13 19:28:34 +0100

    fix(serializer): remove custom serializer
    Incompatible with serialization into a page.

--- SpringBoot/src/main/java/io/github/aveuiller/experiment/delivery/DeliveryBody.java
+++ SpringBoot/src/main/java/io/github/aveuiller/experiment/delivery/DeliveryBody.java
@@ -9,17 +9,17 @@ import java.io.IOException;
 
 @JsonComponent
 public class DeliveryBody {
-    public static class Serializer extends JsonSerializer<Delivery> {
-        @Override
-        public void serialize(Delivery value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-            gen.writeStartObject();
-            gen.writeFieldName("id");
-            gen.writeNumber(value.getId());
-            gen.writeFieldName("location");
-            gen.writeString(value.getLocation());
-            gen.writeFieldName("state");
-            gen.writeString(value.getState().name());
-            gen.close();
-        }
-    }
+//    public static class Serializer extends JsonSerializer<Delivery> {
+//        @Override
+//        public void serialize(Delivery value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+//            gen.writeStartObject();
+//            gen.writeFieldName("id");
+//            gen.writeNumber(value.getId());
+//            gen.writeFieldName("location");
+//            gen.writeString(value.getLocation());
+//            gen.writeFieldName("state");
+//            gen.writeString(value.getState().name());
+//            gen.close();
+//        }
+//    }
 }
