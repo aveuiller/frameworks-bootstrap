2a2b579bd8b88363de0c421b12cb5e07c9be1b5b
Antoine Veuiller <veuiller.a@sfeir.com>
2021-05-28 14:25:41 +0200

    feat(graphql): add spring boot initalizr project

---
+++ GraphQL/spring-boot-integration/src/test/java/io/github/aveuiller/graphql/springbootintegration/SpringBootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aveuiller.graphql.springbootintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
